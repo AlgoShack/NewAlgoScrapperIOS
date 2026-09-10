461ae86392450c57554086acecd2feec7f3976a5
Prashantkage <prashant.kage@algoshack.com>
2026-09-10 14:52:14 +0530

    create feature fixed

--- src/main.js
+++ src/main.js
@@ -1933,13 +1933,7 @@
         {
           label: 'View',
           submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
             { role: 'toggledevtools' },
-            { type: 'separator' },
-            { role: 'resetzoom' },
-            { role: 'zoomin' },
-            { role: 'zoomout' },
             { type: 'separator' },
             { role: 'togglefullscreen' }
           ]
